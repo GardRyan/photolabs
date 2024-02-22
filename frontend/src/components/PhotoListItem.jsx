34b9f21fde37220cd221ee747a518b01767ff871
GardRyan <res.gardner@gmail.com>
2024-02-22 15:35:15 -0500

    added function: photolistitem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,18 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+ return (
+  <div key={id}>
+    <img src={imageSource} />
+    <img src={profile} />
+    <span>
+    {username},
+    {location.city},
+    {location.country}
+    </span>
+  </div>
+ );
 };
 
 export default PhotoListItem;
