0605e1c1cc05d29bc638f19e623bf870e43df7bd
GardRyan <res.gardner@gmail.com>
2024-02-22 22:50:42 -0500

    added classNames so styling understands what it's styling.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,17 +3,20 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
- return (
-  <div key={props.id}>
-    <img src={props.imageSource} />
-    <img src={props.profile} />
-    <span>{props.username} </span>
-    <span>
-    {props.location.city},
-     {props.location.country}
-    </span>
-  </div>
- );
+  return (
+    <div className="photo-list_item" key={props.id}>
+      <img className="photo-list_image" src={props.imageSource} />
+      <div className="photo-list_user-details">
+        <img className="photo-list_user-profile" src={props.profile} />
+        <div className="photo-list_user-info">
+          <span>{props.username} </span>
+          <span className="photo-list_user-location">
+            {props.location.city},{props.location.country}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
