9a60bd6cd4d2d1ebf3c45b891db47f010b0848f8
GardRyan <res.gardner@gmail.com>
2024-02-23 21:20:18 -0500

    PhotoList is working with PhotoListItem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,25 +5,12 @@ import "./App.scss";
 
 // Note: Rendering a single component to build components in isolation
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
 const App = () => {
-  const photos = [...Array(3)];
-
+  // const photos = [...Array(3)];
+  
   return (
     <div className="App">
-      {photos.map((photo, index) => (
-        <PhotoListItem key={index} {...sampleDataForPhotoListItem} />
-      ))}
+     <PhotoList />
     </div>
   );
 };
