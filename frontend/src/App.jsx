1fe25073109cde247cad4d648070aaa0dbb64028
GardRyan <res.gardner@gmail.com>
2024-02-24 13:22:08 -0500

    passing props from parent to child with mock data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 import HomeRoute from "routes/HomeRoute";
 import "./App.scss";
+import topics from "mocks/topics";
+import photos from "mocks/photos";
 
 // Note: Rendering a single component to build components in isolation
 
-const App = () => {
+const App = ({topics, photos}) => {
   // const photos = [...Array(3)];
   
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute topics={topics} photos={photos}/>
     </div>
   );
 };
