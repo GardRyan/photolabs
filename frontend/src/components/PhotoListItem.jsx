04d23e76e89589177f994c2a17e68fba17ffafe8
GardRyan <res.gardner@gmail.com>
2024-02-23 19:20:48 -0500

    createed photos.map to loop through photos and return an array or photos for display

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,18 +3,24 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
+  const photos = new Array(3);
+
   return (
-    <div className="photo-list_item" key={props.id}>
-      <img className="photo-list_image" src={props.imageSource} />
-      <div className="photo-list_user-details">
-        <img className="photo-list_user-profile" src={props.profile} />
-        <div className="photo-list_user-info">
-          <span>{props.username} </span>
-          <span className="photo-list_user-location">
-            {props.location.city},{props.location.country}
-          </span>
+    <div>
+      {photos.map((photo, index) => (
+        <div className="photo-list_item" key={props.id}>
+          <img className="photo-list_image" src={props.imageSource} />
+          <div className="photo-list_user-details">
+            <img className="photo-list_user-profile" src={props.profile} />
+            <div className="photo-list_user-info">
+              <span>{props.username} </span>
+              <span className="photo-list_user-location">
+                {props.location.city},{props.location.country}
+              </span>
+            </div>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
