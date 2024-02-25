7da0e90b15c6e21301506cbdc56b9059f4c2e31a
GardRyan <res.gardner@gmail.com>
2024-02-25 15:02:13 -0500

    passed params, turned object into array and passed key/value pairs to similarphotos

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,14 +2,25 @@ import React from "react";
 
 import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
+import PhotoList from "components/PhotoList";
 
-const PhotoDetailsModal = ({closeModal, selectedPhoto, setSelectedPhoto}) => {
-  
+const PhotoDetailsModal = ({
+  closeModal,
+  selectedPhoto,
+  setSelectedPhoto,
+  similarPhotos,
+}) => {
+
+  const similarPhotosValues = Object.entries(similarPhotos);
+
   return (
     <div className="photo-details-modal">
       <div className="photo-details-modal__top-bar">
-        <button className="photo-details-modal__close-button" onClick={closeModal} >
-          <img src={closeSymbol}  alt="close symbol" />
+        <button
+          className="photo-details-modal__close-button"
+          onClick={closeModal}
+        >
+          <img src={closeSymbol} alt="close symbol" />
         </button>
       </div>
       <div className="photo-details-modal__images">
@@ -20,7 +31,9 @@ const PhotoDetailsModal = ({closeModal, selectedPhoto, setSelectedPhoto}) => {
           />
         )}
       </div>
-      <div className="photo-details-modal__header">{/* Header content ??? */}</div>
+      <div className="photo-details-modal__header">
+        {/* Header content ??? */}
+      </div>
       <div className="photo-details-modal__photographer-details">
         {selectedPhoto.user && (
           <>
@@ -36,6 +49,14 @@ const PhotoDetailsModal = ({closeModal, selectedPhoto, setSelectedPhoto}) => {
             </div>
           </>
         )}
+        <div>
+          <div className="photo-details-modal__similar_photos">
+            <span>Similar Photos</span>
+            {similarPhotosValues && similarPhotosValues.map(([photoId, photo]) => (
+              <img key={photoId} src={photo.urls.regular} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
