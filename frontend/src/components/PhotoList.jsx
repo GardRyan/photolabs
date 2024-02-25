5bc9f152964963db272f3f87ae9672d7815b5007
GardRyan <res.gardner@gmail.com>
2024-02-25 16:31:38 -0500

    linted -no changes

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -5,16 +5,17 @@ import "../styles/PhotoList.scss";
 const PhotoList = ({ photos, toggleFavourite, favourites, openModal }) => {
   return (
     <ul className="photo-list">
-      {photos && photos.map((photo) => (
-        <li key={photo.id} className="photo-list-item">
-          <PhotoListItem
-            photo={photo}
-            toggleFavourite={toggleFavourite}
-            isFavourite={favourites}
-            openModal={openModal}
-          />
-        </li>
-      ))}
+      {photos &&
+        photos.map((photo) => (
+          <li key={photo.id} className="photo-list-item">
+            <PhotoListItem
+              photo={photo}
+              toggleFavourite={toggleFavourite}
+              isFavourite={favourites}
+              openModal={openModal}
+            />
+          </li>
+        ))}
     </ul>
   );
 };
